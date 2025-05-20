9b30290133cf92557a8f6e1bc56a217cf52f96ab
Nico <nikolasleonard@gmail.com>
2025-05-20 16:13:51 -0300

    creado proyecto y terminado ejercicio 1

---
+++ project1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//codigo de prueba
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:" + b);
+/// /// /// /// ///
+//Ejercicio 1
+Console.WriteLine("Ingrese un numero distinto de cero");
+float Fnumero;
+string? dato = Console.ReadLine();
+if (float.TryParse(dato,out Fnumero) && Fnumero != 0)
+{
+    Fnumero = 1 / Fnumero;
+
+    Console.WriteLine($"El numero es: {Fnumero}");
+}
+else
+{
+    Console.WriteLine("El numero debe ser distinto de 0");
+}
