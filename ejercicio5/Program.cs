12058912a4cbc8cb30d2451aa73ce96fa2ae5d5a
Nico <nikolasleonard@gmail.com>
2025-06-21 00:57:12 -0300

    agregado proyecto aparte del ejercicio 5

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+//expresion regular para validar mails
+string[] arregloDeMails = ["correoinvalido.com", "correovalido@algo.com", "otroinvalido@algo", "otro_valido@algo.net"];
+string expresionRegularParaEmail = @"^\S+@\S+\.\S{2,}$";
+foreach (string mail in arregloDeMails)
+{
+    if (Regex.IsMatch(mail, expresionRegularParaEmail))
+        Console.WriteLine($"{mail} es un mail valido.");
+    else
+        Console.WriteLine($"{mail} no es un mail valido");
+}
+Console.WriteLine("--------------");
+
+string[] arregloDeDirecciones = ["https://www.google.com", "www.google.com","google.com", "com.google", "Avenida independencia 1800","https://facetvirtual.facet.unt.edu.ar/course/view.php?id=115"];
+string expresionRegularParaDireccionesWeb = @"^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$";
+foreach (string direccion in arregloDeDirecciones)
+{
+    if (Regex.IsMatch(direccion, expresionRegularParaDireccionesWeb))
+        Console.WriteLine($"{direccion} es una direccion web valida.");
+    else
+        Console.WriteLine($"{direccion} no es una direccion web valida");
+}
